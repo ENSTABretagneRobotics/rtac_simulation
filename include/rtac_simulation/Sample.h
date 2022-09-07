9456d0a3c1abd370669578432c2d112afc52411a
Pierre Narvor <pierre.narvor@ensta-bretagne.fr>
2022-09-07 15:27:55 +0200

    Updated Sample with template type position (indented for CUDA float2/3/4)

--- include/rtac_simulation/Sample.h
+++ include/rtac_simulation/Sample.h
@@ -4,7 +4,7 @@
 #include <iostream>
 #include <array>
 
-#include <rtac_base/cuda/utils.h>
+#include <rtac_simulation/common.h>
 
 namespace rtac { namespace simulation {
 
@@ -14,6 +14,10 @@ namespace rtac { namespace simulation {
  * For acoustic simulation, each contribution to the simulated sensor output is
  * represented by a complex response and a 2D location.
  *
+ * The PointT type is expected to be a CUDA vector type such as float2/3/4. It
+ * represents a 3D location in space expressed by default in cartesian
+ * coordinates (but are used as poler coordinates in some conditions).
+ *
  * The API of the rtac_simulation package is fully templated and the user can
  * replace this type with one of its own. However, some contrains have to be
  * respected to be fully compatible with the rtac_simulation API:
@@ -22,24 +26,23 @@ namespace rtac { namespace simulation {
  * - Must implement operator*= and operator* at least with float and float2
  *   types (float2 type is to be considered a complex number).
  */
-template <typename T>
-struct Sample2D
+template <typename T, typename P>
+struct Sample
 {
-    using Scalar   = T;
-    using Datum    = rtac::cuda::Complex<T>;
+    using Scalar = T;
+    using Point  = P;
 
-    rtac::cuda::Complex<T> datum;
-    T x;
-    T y;
+    Complex<T> datum;
+    Point      position;
 
-    Sample2D<T>& operator*=(T a) {
+    Sample<T,P>& operator*=(T a) {
         datum *= a;
         return *this;
     }
 
-    Sample2D<T>& operator*=(float2 a) {
-        datum = Datum{datum.real()*a.x - datum.imag()*a.y,
-                      datum.real()*a.y + datum.imag()*a.x};
+    Sample<T,P>& operator*=(float2 a) {
+        datum = Complex<T>{datum.real()*a.x - datum.imag()*a.y,
+                           datum.real()*a.y + datum.imag()*a.x};
         return *this;
     }
 };
@@ -47,24 +50,24 @@ struct Sample2D
 }; //namespace simulation
 }; //namespace rtac
 
-template <typename T, typename T2>
-inline rtac::simulation::Sample2D<T> operator*(const rtac::simulation::Sample2D<T>& s, T2 a)
+template <typename T, typename P, typename T2>
+inline rtac::simulation::Sample<T,P> operator*(const rtac::simulation::Sample<T,P>& s, T2 a)
 {
-    rtac::simulation::Sample2D<T> res(s);
+    rtac::simulation::Sample<T,P> res(s);
     res *= a;
     return res;
 }
-template <typename T, typename T2>
-inline rtac::simulation::Sample2D<T> operator*(T2 a, const rtac::simulation::Sample2D<T>& s)
+template <typename T, typename P, typename T2>
+inline rtac::simulation::Sample<T,P> operator*(T2 a, const rtac::simulation::Sample<T,P>& s)
 {
     return s * a;
 }
 
-template <typename T>
-inline std::ostream& operator<<(std::ostream& os, const rtac::simulation::Sample2D<T>& s)
+template <typename T, typename P>
+inline std::ostream& operator<<(std::ostream& os, const rtac::simulation::Sample<T,P>& s)
 {
-    os << "(" << s.datum().real() << ", " << s.datum.imag << "),("
-       << s.x << ", " << s.y << ")";
+    os << "(" << s.datum.real() << ", " << s.datum.imag << "),("
+       << s.position << ")";
     return os;
 }
 
